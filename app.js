f89aab32ae1c3c82ddd976d3b221d40ee8f42844
athira88-dev <athirabmurali@gmail.com>
2025-06-22 22:01:17 +0100

    Add app.js

--- app.js
+++ app.js
@@ -2,12 +2,34 @@ const express=require('express')
 const app=express()
 const path=require('path')
 const env=require('dotenv').config();// Loads environment variables
+const session=require('express-session')//Importing the express-session middleware to manage user sessions in the app
+const passport=require('./config/passport')
 const db=require('./config/db')  // Connects to MongoDB
 // This file will contain all the route handlers related to user-facing pages or APIs.
 const userRouter=require('./routes/userRouter')
+const adminRouter=require('./routes/adminRouter')
 db() // Call the DB connection function
 app.use(express.json())// Parse incoming JSON data in request bodies
 app.use(express.urlencoded({extended:true}))// Parse URL-encoded data (e.g., from HTML form submissions)
+app.use(session({
+    secret:process.env.SESSION_SECRET,
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+    secure:false,
+    httpOnly:true,
+    maxAge:72*60*60*1000
+    }
+}))
+
+app.use(passport.initialize())// sets up Passport.
+app.use(passport.session()) //integrates Passport with express-session for persistent login.
+
+
+app.use((req,res,next)=>{
+    res.set('cache-control','no-store')
+    next()
+})
 
 app.set('view engine','ejs')
 // Set multiple directories for views — one for user views and one for admin views
@@ -16,6 +38,7 @@ app.set('views',[path.join(__dirname,'views/user'),path.join(__dirname,'views/ad
 app.use(express.static(path.join(__dirname, 'public')));
 // Use userRouter to handle routes for user
 app.use('/',userRouter)
+app.use('/admin',adminRouter)
 
 
 
